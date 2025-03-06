6cef7643a4de8580958260f9bf592a0331f48f4b
alejandru00 <alejandrolliber2004@gmail.com>
2025-03-06 12:36:58 +0100

    dim geo

--- docs/helpful queries/Dim_geo.sql
+++ docs/helpful queries/Dim_geo.sql
@@ -1,11 +1,12 @@
 -- contruccion tabla dimensiona GEO
 
-SELECT [TIENDA_ID]
-    , tienda.[PROVINCIA_ID]
-    , [PROV_DESC]
-    , tienda.[ZONA_ID]
-    , [ZONA]
-    , [TIENDA_DESC]
+SELECT 
+    TIENDA_ID,
+    tienda.PROVINCIA_ID,
+    PROV_DESC,
+    tienda.ZONA_ID,
+    ZONA,
+    TIENDA_DESC
 FROM
     [DATAEX].[011_tienda] tienda
 LEFT JOIN 
