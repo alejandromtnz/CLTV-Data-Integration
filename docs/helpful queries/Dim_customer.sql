fbf15422016b5c36eb51ca97e5f1ef4cae97498f
alejandru00 <alejandrolliber2004@gmail.com>
2025-03-06 12:39:23 +0100

    dim customer

--- docs/helpful queries/Dim_customer.sql
+++ docs/helpful queries/Dim_customer.sql
@@ -1,30 +1,29 @@
 -- contruccion tabla dimensiona cliente
 
 SELECT 
-    Customer_ID INT
-    , Edad INT
-    , Fecha_nacimiento DATE
-    , GENERO TEXT
+    Customer_ID INT,
+    Edad INT,
+    Fecha_nacimiento DATE,
+    GENERO TEXT,
     
-    , CAST(codigopostalid AS INT) AS CP
-    , poblacion TEXT
-    , provincia TEXT
+    CAST(codigopostalid AS INT) AS CP,
+    poblacion TEXT,
+    provincia TEXT,
 
-    , STATUS_SOCIAL TEXT
-    , [RENTA_MEDIA_ESTIMADA] INT
-    , [ENCUESTA_ZONA_CLIENTE_VENTA] INT
-    , [ENCUESTA_CLIENTE_ZONA_TALLER] INT
+    STATUS_SOCIAL TEXT,
+    [RENTA_MEDIA_ESTIMADA] INT,
+    [ENCUESTA_ZONA_CLIENTE_VENTA] INT,
+    [ENCUESTA_CLIENTE_ZONA_TALLER] INT,
 
     -- que es U2, Renta_Media, F2, Count?
-    , [Renta_Media] INT
+    [Renta_Media] INT,
     -- hace falta A1, A2, A3, etc o solo con A sirve?
 
     -- tampoco pondria las letras pq no aparecen en que letra estan 
     -- o si si dps con la renta media lo clasificaremos
     -- que son los campos A, B, C, D, E, F, G, H, I, J, K?
-    , [A], [B], [C], [D], [E], [F], [G], [H], [I], [J], [K], [U2]
-    , [A1]
-
+    [A], [B], [C], [D], [E], [F], [G], [H], [I], [J], [K], [U2],
+    [A1]
 
 FROM
     [DATAEX].[019_Mosaic] mosaic
