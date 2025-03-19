5607a1deb1f0d88147399a474fbc54878c8f992d
alejandru00 <alejandrolliber2004@gmail.com>
2025-03-19 15:15:54 +0100

    error dim producto solucionado

--- src/sql/Dim_product.sql
+++ src/sql/Dim_product.sql
@@ -3,26 +3,26 @@
 SELECT 
     -- 006: producto (costes + categoria_producto + fuel)
     Id_Producto,
-    Kw INT,
-    TRANSMISION_ID TEXT, -- hacefalta? para que sirve si no la podemos unir?
+    CAST(Kw AS INT) AS Kw,
+    CAST(TRANSMISION_ID AS TEXT) AS TRANSMISION_ID, -- hacefalta? para que sirve si no la podemos unir?
 
     ---- 014: categoria_producto
-    producto.CATEGORIA_ID,
-    Grade_ID INT,       -- va desde 0 a 4 referenciando 'equipemiento': hace falta equipamiento?
+    CAST(producto.CATEGORIA_ID AS INT) AS CATEGORIA_ID,
+    CAST(Grade_ID AS INT) AS Grade_ID,       -- va desde 0 a 4 referenciando 'equipemiento': hace falta equipamiento?
     Equipamiento,
 
     ---- 015: fuel
-    producto.Fuel_ID TEXT,
+    producto.Fuel_ID,
     FUEL,
 
     ---- 007: costes
-    producto.Modelo TEXT,
-    Margen,
-    Costetransporte,
-    Margendistribuidor,
-    GastosMarketing,
-    Mantenimiento_medio,
-    Comisión_Marca
+    producto.Modelo,
+    CAST([Margen] AS INT) AS Margen,
+    CAST([Costetransporte] AS INT) AS Costetransporte,
+    CAST([Margendistribuidor] AS INT) AS Margendistribuidor,
+    CAST([GastosMarketing] AS INT) AS GastosMarketing,
+    CAST([Mantenimiento_medio] AS INT) AS Mantenimiento_medio,
+    CAST([Comisión_Marca] AS INT) AS Comisión_Marca
 
 FROM
     [DATAEX].[006_producto] producto
